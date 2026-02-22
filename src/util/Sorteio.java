ed1463e0c83c9636046aa9c262ead1e7d260f585
gustavo-gml <gustavomartinslima06@gmail.com>
2026-02-22 15:36:36 -0300

    fix: corrige formatação da distribuição de cartas e loop de nova partida

--- src/util/Sorteio.java
+++ src/util/Sorteio.java
@@ -7,34 +7,28 @@ public class Sorteio {
     
     // Função que retorna um número de 1 a 13
     public static int sortearCarta(ArrayList<Integer> baralho, ArrayList<Integer> cartasUsadas) {
+        Integer pilha = baralho.getFirst();
+
+        cartasUsadas.add(baralho.getFirst());
+        baralho.remove(baralho.getFirst());
         
-        if (baralho.isEmpty()) {
-            System.out.println("Reembaralhando as cartas...");
-            baralho.addAll(cartasUsadas);
-            cartasUsadas.clear();
-            java.util.Collections.shuffle(baralho);
-        }
-        
-        Random random = new Random();
-        int indiceSorteado = random.nextInt(baralho.size());
-        
-        int cartaSorteada = baralho.get(indiceSorteado);
-        baralho.remove(indiceSorteado);
-        cartasUsadas.add(cartaSorteada);
-        
-        return cartaSorteada;
+        return pilha;
     }
     
     // Cria baralho com números de 1 a 13 (4 de cada)
     public static ArrayList<Integer> criarBaralho() {
+
         ArrayList<Integer> baralho = new ArrayList<>();
-        
         for (int i = 1; i <= 13; i++) {
             for (int j = 0; j < 4; j++) {
                 baralho.add(i);
             }
         }
-        
         return baralho;
+    }
+
+    public static void reiniciarBaralho(ArrayList<Integer> baralho, ArrayList<Integer> cartasUsadas) {
+        baralho.addAll(cartasUsadas);
+        cartasUsadas.clear();
     }
 }
